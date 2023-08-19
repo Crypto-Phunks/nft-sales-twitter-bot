90f546776f9fc0fab82a6df3814e6a6f8dba3032
Simon <simon.oualid+phunk@gmail.com>
2023-08-19 23:02:28 +0200

    added log rotation for winston

--- src/logging.utils.ts
+++ src/logging.utils.ts
@@ -1,4 +1,5 @@
 import winston from 'winston';
+import DailyRotateFile from 'winston-daily-rotate-file'
 
 export function createLogger(service:string) {
   return winston.createLogger({
@@ -12,7 +13,13 @@ export function createLogger(service:string) {
     defaultMeta: { service: 'base-service' },
     transports: [
       new winston.transports.File({ filename: 'error.log', level: 'error' }),
-      new winston.transports.File({ filename: 'combined.log' }),
+      new DailyRotateFile({
+        filename: 'combined-%DATE%.log',
+        datePattern: 'YYYY-MM-DD-HH',
+        zippedArchive: true,
+        maxSize: '20m',
+        maxFiles: '14d'
+      }),
       new winston.transports.Console({}),
     ],
   });
